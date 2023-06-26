4873ffb0669e7bf557f545fbd9c9d5dcb4057761
leejunseok <wnstjr120422@naver.com>
2023-06-26 22:29:02 +0900

    add: Project build

---
+++ src/main/java/com/example/CafeTour/CafeTourApplication.java
@@ -0,0 +1,13 @@
+package com.example.CafeTour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeTourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CafeTour/CafeTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CafeTour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
