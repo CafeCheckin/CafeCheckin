fd47388a95b9c6b310d307750726d74fad516622
leejunseok <wnstjr120422@naver.com>
2023-07-18 23:00:28 +0900

    modify: formlogin()

---
+++ src/main/java/com/example/CafeTour/config/SpringConfig.java
@@ -0,0 +1,57 @@
+package com.example.CafeTour.config;
+
+import lombok.Builder;
+import org.springframework.boot.web.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@Configuration
+@EnableWebSecurity
+public class SpringConfig  {
+/*
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().disable()
+                .authorizeHttpRequests(request -> request
+                        .antMatchers("/home","/").permitAll()
+                        .anyRequest().authenticated()	// 어떠한 요청이라도 인증필요
+                )
+                .formLogin(login -> login	// form 방식 로그인 사용
+                        .defaultSuccessUrl("/kakao", true)	// 성공 시 dashboard로
+                        .permitAll()	// 대시보드 이동이 막히면 안되므로 얘는 허용
+                )
+                .logout(withDefaults());	// 로그아웃은 기본설정으로 (/logout으로 인증해제)
+
+        return http.build();
+    }*/
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable().cors().disable()
+                .authorizeRequests()
+                    .antMatchers("/home","/","/JoinForm").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+
+                .formLogin()
+                    .loginPage("/LoginForm").permitAll()
+                    .loginProcessingUrl("/LoginForm")
+                    .defaultSuccessUrl("/kakao")// 패스워드 파라미터명 설정
+                      .and()
+
+                .logout()
+                    .permitAll();
+        return http.build();
+    }
+}
+
--- src/main/java/com/example/CafeTour/controller/HomeController.java
+++ src/main/java/com/example/CafeTour/controller/HomeController.java
@@ -2,20 +2,33 @@ package com.example.CafeTour.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class HomeController {
-    @GetMapping("kakao")
+    @RequestMapping("/kakao")
     public String home(){
         return "kakao";
     }
 
-    @GetMapping("home")
+    @GetMapping("/home")
     public String h1(){
         return "home";
     }
 
-    @GetMapping("login")
+    @RequestMapping("/LoginForm")
+    public String loginForm(){
+        return "LoginForm";
+    }
+
+    @GetMapping("/JoinForm")
+    public String joinForm(){
+        return "JoinForm";
+    }
+
+    @GetMapping("/login")
     public String login(){
         return "login";
     }
